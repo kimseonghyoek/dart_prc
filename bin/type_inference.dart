eb0f84824f230c627b0507f0304fff1a177a5d58
KSH <cilo1223@naver.com>
2021-08-10 00:07:48 +0900

    type inference

---
+++ bin/type_inference.dart
@@ -0,0 +1,7 @@
+var i = 10; // type of int
+var d = 10.0; // type of double
+var s = 'hello'; // type of String
+var s2 = "hello"; // type of String
+var b = true; // type of bool
+var b2 = i < 10; // type of bool
+var b3 = s.isEmpty; // type of bool
