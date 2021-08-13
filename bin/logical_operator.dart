a8babb57226d518fed02ca8d8175c1bb9e1c7b25
KSH <cilo1223@naver.com>
2021-08-14 02:03:18 +0900

    logical operator

---
+++ bin/logical_operator.dart
@@ -0,0 +1,18 @@
+void main() {
+  print(true && true);
+  print(true && false);
+  print(false && false);
+
+  print(true || true);
+  print(true || false);
+  print(false || false);
+
+  print(true == true);
+  print(true == false);
+  print(false == false);
+
+  print(true != true);
+  print(true != false);
+  print(false != false);
+
+}
