16df82e8ea573511e6ab21611f544cadb6fea3ac
KSH <cilo1223@naver.com>
2021-09-11 00:52:16 +0900

    update

---
+++ bin/enum_Type.dart
@@ -0,0 +1,14 @@
+enum Status {
+  login, logout
+}
+
+var authStatus = Status.logout;
+
+switch(authStatus) {
+  case Status.login:
+    print('로그인');
+    break;
+  case Status.logout:
+    print('로그아웃');
+    break;
+}
