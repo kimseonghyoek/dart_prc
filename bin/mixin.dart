74de7b70737037cc08980ebefd90eecbf2db5c81
KSH <cilo1223@naver.com>
2021-09-11 00:32:33 +0900

    add mixin

---
+++ bin/mixin.dart
@@ -0,0 +1,39 @@
+import 'inheritance.dart';
+
+abstract class Monster {
+  void attack();
+}
+
+class Goblin implements Monster {
+  @override
+  void attack() {
+    print('고블린 어택');
+  }
+}
+
+class Bat implements Monster {
+  @override
+  void attack() {
+    print('할퀴기!');
+  }
+}
+
+abstract class Flyable {
+  void fly();
+}
+
+class Bat1 implements Monster, Flyable {
+  @override
+  void attack() {
+    print('할퀴기');
+  }
+
+  @override
+  void fly() {
+    print('펄럭펄럭');
+  }
+}
+
+class DarkGoblin extends Goblin with Hero {
+
+}
